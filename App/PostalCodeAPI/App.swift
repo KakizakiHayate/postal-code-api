cf0ee14d11f134c318874fda4f1027b530a566ef
kakizaki hayate <hayate.h.0704@gmail.com>
2023-12-06 01:22:26 +0900

    add: 初期生成ファイルを含めたモジュールを追加

---
+++ App/PostalCodeAPI/App.swift
@@ -0,0 +1,18 @@
+//
+//  PostalCodeAPIApp.swift
+//  PostalCodeAPI
+//
+//  Created by 柿崎逸 on 2023/12/05.
+//
+
+import SwiftUI
+import Home
+
+@main
+struct PostalCodeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
