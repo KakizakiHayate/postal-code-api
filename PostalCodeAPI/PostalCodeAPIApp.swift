be5119ec934c8e62790f5aa0036f183fa820acc2
kakizaki hayate <hayate.h.0704@gmail.com>
2023-12-05 14:19:06 +0900

    add: Xcode initial file

---
+++ PostalCodeAPI/PostalCodeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostalCodeAPIApp.swift
+//  PostalCodeAPI
+//
+//  Created by 柿崎逸 on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct PostalCodeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
