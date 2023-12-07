ea7519cef40bcedd20e637d1f01a5149e17e248e
kakizaki hayate <hayate.h.0704@gmail.com>
2023-12-07 13:54:43 +0900

    add: APIのレスポンスを表示する画面を追加

---
+++ Sources/AddressResultFeature/AddressResultView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 柿崎逸 on 2023/12/06.
+//
+
+import SwiftUI
+//import ComposableArchitecture
+
+public struct AddressResultView: View {
+    public init() {}
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AddressResultView()
+}
