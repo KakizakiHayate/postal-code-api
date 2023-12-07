a6f8f9c4e57b06a5c05dba76efb42b050cd6735b
kakizaki hayate <hayate.h.0704@gmail.com>
2023-12-07 13:52:30 +0900

    update: AppのContentViewをHomeViewに変更

--- App/PostalCodeAPI/App.swift
+++ App/PostalCodeAPI/App.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 import Home
+import ComposableArchitecture
 
 @main
-struct PostalCodeAPIApp: App {
+struct PostalCodeAPIApp: SwiftUI.App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView(
+                store: .init(
+                    initialState: Home.State()
+                ) {
+                    Home() // ここがないとStateやActionが処理されない
+                }
+            )
         }
     }
 }
